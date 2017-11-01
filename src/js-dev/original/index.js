27dffba719722e74f7c671b3655c0b2a7be45af7
Billy.Liao <billfulo@gmail.com>
2017-11-01 18:19:16 +0800

    test fix IE

--- src/js-dev/original/index.js
+++ src/js-dev/original/index.js
@@ -23,7 +23,6 @@ $(document).ready(function () {
     var animeIndex;
     var toSkip = false;
     var toFire = true;
-    var skiped = false;
 
     var scroll_now;
     var read_progress = 10;
@@ -1432,9 +1431,6 @@ $(document).ready(function () {
                     "display": "block",
                     "color": "white"
                 });
-                if (skiped == true) {
-                    console.log("skiped")
-                }
             }
             if (index == 2) {
                 walk1.src = "src/image/animate-sprite/stage-3-walk1.png";
